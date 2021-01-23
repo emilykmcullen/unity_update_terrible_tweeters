2ac336bea0349b74be4e01d142f371398432ce4e
emilykmcullen <emilycullen135@gmail.com>
2021-01-23 11:46:57 +0000

    bird can hit and topple crates

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,35 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    [SerializeField] float _launchForce = 500;
+
     Vector2 _startPosition;
+    Rigidbody2D _rigidbody2D;
+    SpriteRenderer _spriteRenderer;
+
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        _startPosition = GetComponent<Rigidbody2D>().position;
-        GetComponent<Rigidbody2D>().isKinematic = true;
+        _startPosition = _rigidbody2D.position;
+        _rigidbody2D.isKinematic = true;
 
     }
 
     void OnMouseDown()
     {
-        GetComponent<SpriteRenderer>().color = Color.red;
+        _spriteRenderer.color = Color.red;
     }
 
     void OnMouseUp()
     {
-        Vector2 currentPosition = GetComponent<Rigidbody2D>().position;
+        Vector2 currentPosition = _rigidbody2D.position;
         Vector2 direction = _startPosition - currentPosition;
         direction.Normalize();
 
-        GetComponent<Rigidbody2D>().isKinematic = false;
-        GetComponent<Rigidbody2D>().AddForce(direction * 500);
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * _launchForce);
 
-        GetComponent<SpriteRenderer>().color = Color.white;
+        _spriteRenderer.color = Color.white;
     }
 
     void OnMouseDrag()
@@ -43,5 +54,18 @@ public class Bird : MonoBehaviour
     {
         
 
+    }
+     
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ResetAfterDelay());
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        _rigidbody2D.position = _startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
     }
 }
